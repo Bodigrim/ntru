d9cbe5bb4ab9524fd151d632f83cd1ac538304cb
Bodigrim <andrew.lelechenko@gmail.com>
2020-01-26 19:35:01 +0000

    Replace tabs with spaces

--- examples/sampleGeneralNTRU.hs
+++ examples/sampleGeneralNTRU.hs
@@ -7,20 +7,21 @@ import Math.NTRU
 import Data.Char
 import Data.Maybe
 
+main :: IO ()
 main = do
 
---  let params = genParams "EES401EP1"
-	let params = ParamSet {getN =  401, getP = 3, getQ = 2048, getDf = 113, getDg = 133, getLLen = 1, getDb = 112, getMaxMsgLenBytes = 60, getBufferLenBits = 600, getBufferLenTrits = 400, getDm0 = 113, getShaLvl = 1, getDr = 113, getC = 11, getMinCallsR = 32, getMinCallsMask = 9, getOID = [0,2,4], getPkLen = 114, getBitLvl = 112}
+  --  let params = genParams "EES401EP1"
+  let params = ParamSet {getN =  401, getP = 3, getQ = 2048, getDf = 113, getDg = 133, getLLen = 1, getDb = 112, getMaxMsgLenBytes = 60, getBufferLenBits = 600, getBufferLenTrits = 400, getDm0 = 113, getShaLvl = 1, getDr = 113, getC = 11, getMinCallsR = 32, getMinCallsMask = 9, getOID = [0,2,4], getPkLen = 114, getBitLvl = 112}
 
-	(publicKey, privateKey) <- keyGen params
+  (publicKey, privateKey) <- keyGen params
 
-	let msg = map (fromIntegral . ord) "Hello, World" :: [Integer]
+  let msg = map (fromIntegral . ord) "Hello, World" :: [Integer]
 
-	ct <- encrypt params msg publicKey
+  ct <- encrypt params msg publicKey
 
-	let unencrypted = decrypt params privateKey publicKey ct
+  let unencrypted = decrypt params privateKey publicKey ct
 
-	if (isNothing unencrypted)
-	then print "Encryption Failed"
-	else print $ map (chr . fromIntegral) (fromJust unencrypted)
+  if (isNothing unencrypted)
+  then print "Encryption Failed"
+  else print $ map (chr . fromIntegral) (fromJust unencrypted)
 
--- examples/sampleSpecificNTRU.hs
+++ examples/sampleSpecificNTRU.hs
@@ -8,17 +8,18 @@ import Math.NTRU.EES401EP1
 import Data.Char
 import Data.Maybe
 
+main :: IO ()
 main = do
 
-	(publicKey, privateKey) <- keyGen
+  (publicKey, privateKey) <- keyGen
 
-	let msg = map (fromIntegral . ord) "Hello, World" :: [Integer]
+  let msg = map (fromIntegral . ord) "Hello, World" :: [Integer]
 
-	ct <- encrypt msg publicKey
+  ct <- encrypt msg publicKey
 
-	let unencrypted = decrypt privateKey publicKey ct
+  let unencrypted = decrypt privateKey publicKey ct
 
-	if (isNothing unencrypted)
-	then print "Encryption Failed"
-	else print $ map (chr . fromIntegral) (fromJust unencrypted)
+  if (isNothing unencrypted)
+  then print "Encryption Failed"
+  else print $ map (chr . fromIntegral) (fromJust unencrypted)
 
